97b0d859f7bc1c08631e6ecb365510147da6185f
Jacob Arychuk <jza@linux200.csc.uvic.ca>
2024-10-29 09:50:18 -0700

    refactor: make IPv4 header offset explicit

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ def main():
 			incl_len = int.from_bytes(packet_header[8:12], byteorder=endianness) # Extract incl_len from packet header
 			packet_data = f.read(incl_len) # Read packet data
 
-			source_address = socket.inet_ntoa(packet_data[26:30]) # Extract source address from packet data
-			destination_address = socket.inet_ntoa(packet_data[30:34]) # Extract destination address from packet data
+			ipv4_header_offset = 14
+			source_address = socket.inet_ntoa(packet_data[ipv4_header_offset+12:ipv4_header_offset+16]) # Extract source address from packet data
+			destination_address = socket.inet_ntoa(packet_data[ipv4_header_offset+16:ipv4_header_offset+20]) # Extract destination address from packet data
 			print(f"Packet {packet_number}:")
 			print("Source Address:", source_address)
 			print("Destination Address:", destination_address)
