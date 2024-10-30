090f33cfd848507a63738652a85c7e20d63fd23e
Jacob Arychuk <jza@linux200.csc.uvic.ca>
2024-10-29 21:13:01 -0700

    docs: add docstring and update comments in get_endianness

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import sys
 
 def get_endianness(global_header):
-
-	magic_number = int.from_bytes(global_header[:4], byteorder='big') # Extract magic_number from global header
-
-	if magic_number == 0xa1b2c3d4: # File was written in big-endian format
+	"""Return the endianness of a CAP/PCAP file based on the magic number in its global header."""
+	# Extract magic_number from global header
+	magic_number = int.from_bytes(global_header[:4], byteorder='big')
+	# Determine endianness
+	if magic_number == 0xa1b2c3d4:
 		return 'big'
-	elif magic_number == 0xd4c3b2a1: # File was written in little-endian format
+	elif magic_number == 0xd4c3b2a1:
 		return 'little'
 	else:
 		print("Unsupported file format", file=sys.stderr)
