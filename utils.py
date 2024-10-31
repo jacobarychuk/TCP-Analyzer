a93da4be1b26018bab2e59542b928f3bc93a9134
Jacob Arychuk <jza@linux200.csc.uvic.ca>
2024-10-31 14:25:00 -0700

    refactor: move IPv4 header offset constant to config.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import sys
 import socket
-
-IPV4_HEADER_OFFSET = 14
+import config
 
 
 def get_endianness(global_header):
@@ -20,30 +19,30 @@ def get_endianness(global_header):
 
 def get_source_address(packet_data):
 	"""Extract and return the source IP address from the packet data."""
-	return socket.inet_ntoa(packet_data[IPV4_HEADER_OFFSET+12:IPV4_HEADER_OFFSET+16])
+	return socket.inet_ntoa(packet_data[config.IPV4_HEADER_OFFSET+12:config.IPV4_HEADER_OFFSET+16])
 
 
 def get_destination_address(packet_data):
 	"""Extract and return the destination IP address from the packet data."""
-	return socket.inet_ntoa(packet_data[IPV4_HEADER_OFFSET+16:IPV4_HEADER_OFFSET+20])
+	return socket.inet_ntoa(packet_data[config.IPV4_HEADER_OFFSET+16:config.IPV4_HEADER_OFFSET+20])
 
 
 def get_source_port(packet_data):
 	"""Extract and return the source port from the packet data."""
-	ihl_bytes = (packet_data[IPV4_HEADER_OFFSET] & 0x0F) * 4
+	ihl_bytes = (packet_data[config.IPV4_HEADER_OFFSET] & 0x0F) * 4
 	tcp_header_offset = 14 + ihl_bytes
 	return int.from_bytes(packet_data[tcp_header_offset:tcp_header_offset+2], byteorder='big')
 
 
 def get_destination_port(packet_data):
 	"""Extract and return the destination port from the packet data."""
-	ihl_bytes = (packet_data[IPV4_HEADER_OFFSET] & 0x0F) * 4
+	ihl_bytes = (packet_data[config.IPV4_HEADER_OFFSET] & 0x0F) * 4
 	tcp_header_offset = 14 + ihl_bytes
 	return int.from_bytes(packet_data[tcp_header_offset+2:tcp_header_offset+4], byteorder='big')
 
 def get_flags(packet_data):
 	"""Extract, interpret, and return the flags from the TCP header in the packet data."""
-	ihl_bytes = (packet_data[IPV4_HEADER_OFFSET] & 0x0F) * 4
+	ihl_bytes = (packet_data[config.IPV4_HEADER_OFFSET] & 0x0F) * 4
 	tcp_header_offset = 14 + ihl_bytes
 	flags = int.from_bytes(packet_data[tcp_header_offset+12:tcp_header_offset+14], byteorder='big') # Mask to get the lower 9 bits
 	flag_names = ["FIN", "SYN", "RST", "PSH", "ACK", "URG", "ECE", "CWR", "NS"]
